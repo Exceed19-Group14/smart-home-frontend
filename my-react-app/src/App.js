770e7c8dc0b5d26b489ff9f9eea0042cc1b27ca5
Ratthicha Parinthip <92798644+premepreme@users.noreply.github.com>
2023-02-08 12:05:06 +0700

    add img

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,9 +4,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <main>
+
+      <div className="title">
+        <h1>Smart Home Lighting</h1>
+      </div>
+      <div className='two-col'>
+        <img src='https://greenbeltapts.com/wp-content/uploads/Greenbelt-Apartment-Floor-Plan-A-1-Bedroom-1-Bath.png' width="400"  />
+        <div className='room'>
+          <h2>Kitchen</h2>
+          <h2>Lounge</h2>
+          <h2>Master Bedroom</h2>
+        </div>
+      </div>
+      
+
+    </main>
+
+
   );
 }
 
